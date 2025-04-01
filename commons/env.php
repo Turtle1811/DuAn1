f641c26a7a1d8d676a84722dc8e645dd2286aed6
Turtle1811 <phongrua2002@gmail.com>
2025-04-02 03:50:58 +0700

    Add files via upload

---
+++ commons/env.php
@@ -0,0 +1,14 @@
+<?php
+
+define('BASE_URL' , 'http://localhost/duan1/');
+define('BASE_URL_ADMIN' , 'http://localhost/duan1/admin/');
+
+
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'duan1');
+
+define('PATH_ROOT', __DIR__ . '/../');
